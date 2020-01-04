ab79449dc8ab95d45cedce6fd6494960dfcbde72
adair_chan <adair_chan@hotmail.com>
2020-01-04 22:11:47 +0800

    feat(牛课网SQL实战): 01-查找最晚入职员工所有信息

--- awesome-mysql/newcoder-mysql/src/main/resources/1~10/01-查找最晚入职员工.sql
+++ awesome-mysql/newcoder-mysql/src/main/resources/1~10/01-查找最晚入职员工.sql
@@ -1,2 +1,24 @@
+
+# 01 查找最晚入职员工
+CREATE TABLE `employees` (
+                             `emp_no` int(11) NOT NULL,
+                             `birth_date` date NOT NULL,
+                             `first_name` varchar(14) NOT NULL,
+                             `last_name` varchar(16) NOT NULL,
+                             `gender` char(1) NOT NULL,
+                             `hire_date` date NOT NULL,
+                             PRIMARY KEY (`emp_no`));
+INSERT INTO employees VALUES(10001,'1953-09-02','Georgi','Facello','M','1986-06-26');
+INSERT INTO employees VALUES(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21');
+INSERT INTO employees VALUES(10003,'1959-12-03','Parto','Bamford','M','1986-08-28');
+INSERT INTO employees VALUES(10004,'1954-05-01','Chirstian','Koblick','M','1986-12-01');
+INSERT INTO employees VALUES(10005,'1955-01-21','Kyoichi','Maliniak','M','1989-09-12');
+INSERT INTO employees VALUES(10006,'1953-04-20','Anneke','Preusig','F','1989-06-02');
+INSERT INTO employees VALUES(10007,'1957-05-23','Tzvetan','Zielinski','F','1989-02-10');
+INSERT INTO employees VALUES(10008,'1958-02-19','Saniya','Kalloufi','M','1994-09-15');
+INSERT INTO employees VALUES(10009,'1952-04-19','Sumant','Peac','F','1985-02-18');
+INSERT INTO employees VALUES(10010,'1963-06-01','Duangkaew','Piveteau','F','1989-08-24');
+INSERT INTO employees VALUES(10011,'1953-11-07','Mary','Sluis','F','1990-01-22');
+
 # 最晚入职的当天未必就一个人，也许有多人，使用排序并限制得只能取得指定数量的结果，所以这里利用条件子查询
 select * from employees where hire_date = (select max(hire_date) from employees)
