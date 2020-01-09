e86010a9830a9d50113503a54113551ae469d55d
adair_chan <adair_chan@hotmail.com>
2020-01-09 11:27:17 +0800

    feat(牛课网SQL实战): 19
    查找所有员工的last_name和first_name以及对应的dept_name，也包括暂时没有分配部门的员工

---
+++ awesome-mysql/newcoder-mysql/src/main/resources/11~20/19.sql
@@ -0,0 +1,29 @@
+# 查找所有员工的last_name和first_name以及对应的dept_name，也包括暂时没有分配部门的员工
+CREATE TABLE `departments` (
+`dept_no` char(4) NOT NULL,
+`dept_name` varchar(40) NOT NULL,
+PRIMARY KEY (`dept_no`));
+
+CREATE TABLE `dept_emp` (
+`emp_no` int(11) NOT NULL,
+`dept_no` char(4) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`dept_no`));
+
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+
+select de.last_name, de.first_name, dt.dept_name
+from (select e.last_name, e.first_name, d.dept_no
+    from employees as e
+    left join dept_emp d
+    on e.emp_no = d.emp_no) de
+left join departments dt
+on dt.dept_no = de.dept_no
